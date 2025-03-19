2ef04312ea030661deca62905d532d61cab58aec
chenruchi <voltextra@gmail.com>
2025-03-19 22:03:43 +0700

    EP.11 GUI TKinter
    โปรแกรมบันทึกข้อมูลลง CSV

--- Gui-knowledge.py
+++ Gui-knowledge.py
@@ -1,14 +1,23 @@
 from tkinter import * #import function inside main package B=Button()
 from tkinter import ttk #package ย่อย ttk ที่มี theme สวยกว่า
+import csv
+
+def writecsv(data):
+    # data = ['John',14,500] 
+    with open('knowledge.csv','a',newline='',encoding='utf-8') as file:
+        fw = csv.writer(file) #FW = File Writer
+        fw.writerow(data)
 
 GUI = Tk()
 GUI.title('โปรแกรมบันทึกความรู้ By BottleDev')
 GUI.geometry('500x500')
 
-L1 =ttk.Label(GUI,text='หัวข้อความรู้',font=('Angsana New',18))
+
+L1 = ttk.Label(GUI, text='หัวข้อความรู้',font=('Angsana New',18))
 L1.pack()
 
-E1 = ttk.Entry(GUI,font=('Angsana New',20),width=50)
+v_title = StringVar()
+E1 = ttk.Entry(GUI,textvariable=v_title,font=('Angsana New',20),width=50)
 E1.pack() #(วางไว้ตรงกลาง บนลงล่าง)
 #E1.place(x=300, y=300) #(วางตามตำแหน่ง x y ที่เรากำหนด)
 
@@ -18,7 +27,22 @@ L2.pack()
 T1 = Text(GUI,font=('Angsana New',18), height=8, width=56)
 T1.pack()
 
-B1 = ttk.Button(GUI,text='บันทึก')
+def save():
+    title = v_title.get()
+    textbox = T1.get(1.0,"end-1c")
+    print('-------')
+    print(title)
+    print('-------')
+    print(textbox)
+    print('-------')
+
+    data = [title,textbox]
+    writecsv(data)
+
+    v_title.set('') #clear data after save
+    T1.delete('1.0',END) #clear textboxs
+
+B1 = ttk.Button(GUI,text='บันทึก',command=save)
 B1.pack(pady=10, ipadx=20, ipady=10)
 
 
